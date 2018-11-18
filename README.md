2e2bd6d33fc0494ab7e4596661b27fb16f8bcd77
Elyes Graba <elyesgraba@gatech.edu>
2018-11-18 12:57:09 -0600

    updated readme again, actually fixed typo

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ## Given a subreddit, train up a markov chain model on posts from that subreddit and start imitating posts
 
+### [Inspired by](http://slatestarcodex.com/2018/10/30/sort-by-controversial/)
+
 ### runs on python3
 
 1. `virtualenv -p python3 ShitPoster3000`
@@ -11,3 +13,12 @@
 3. `pip install -r requirements.txt`
 4. to run as CLI: `python3 ShitPoster3000 <optional number of sentences>`
 5. as server: `./startServer.sh`
+
+### If running as a server: URL Params (all optional)
+* `param` - description - `default`
+* `subreddit` - which subreddit to pull posts from - `worldnews`
+* `sort` - sort by new, top, controversial, etc - `controversial`
+* `period` - day, week, month, year, all - `all`
+* `samples` - number of post samples to train the markov chain model on - `500`
+* `sentenceCount` - how many simulated sentences to generate - `1`
+* `useTitles` - whether to use the titles of the posts as training data (uses the posts' body text if set to `false`) - `true`
--- ShitPoster3000Server.py
+++ ShitPoster3000Server.py
@@ -10,11 +10,22 @@ def mimickSubreddit():
     subreddit = params.get('subreddit', 'worldnews')
     sort = params.get('sort', 'controversial')
     period = params.get('period', 'all')
-    samples = params.get('samples', 500)
-    sentenceCount = int(params.get('sentenceCount', 1))
-    useTitles = params.get('useTitles', True)
+    samples = getInt(params, 'samples', 500)
+    sentenceCount = getInt(params, 'sentenceCount', 1)
+    useTitles = isTrue(params.get('useTitles', True))
     if sort != 'top' and sort != 'controversial':
         model = train(subreddit, useTitles=useTitles, sort=sort, numSamples=samples)
     else:
-        model = train(subreddit, useTitles=useTitles, sort=sort, params={'t': period}, numamples=samples)
+        model = train(subreddit, useTitles=useTitles, sort=sort, params={'t': period}, numSamples=samples)
     return jsonify([model.make_sentence(tries=100) for _ in range(sentenceCount)])
+
+def getInt(params, key, default):
+    try:
+        return int(params.get(key, default))
+    except ValueError:
+        return default
+
+def isTrue(val):
+    if type(val) == bool:
+        return val
+    return val.lower() == 'true'
